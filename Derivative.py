063f928085132f3a5a84b99a21f13e44533c070b
Allentrium <122357716+Algoatrithm@users.noreply.github.com>
2026-04-09 23:03:33 +0800

    Add files via upload

---
+++ Derivative.py
@@ -0,0 +1,54 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+'''
+example: 
+
+2x^3 + 3x -8
+coeficient = [2, 3, -8]
+exponents = [3, 1, 0]
+
+'''
+# Single Derivative
+def derive_power(coeficients, exponents):
+    ret_term = []
+    ret_exp = []
+    for i in range(len(coeficients)):
+        ret_term.append(coeficients[i] * exponents[i])
+        ret_exp.append(exponents[i] - 1)
+    return [ret_term, ret_exp]
+    
+''' Take all orders of derivative and evaluate using x_val, returns an array of all derivative'''
+def f_of_x(coef, exp, x_val):
+    val = []
+    for i in range(max(exp)):
+        eval = 0
+        for i in range(len(coef)):
+            if (exp[i] < 0):
+                continue
+            eval = eval + coef[i]*(x_val**exp[i]);
+            print(str(coef[i]*(x_val**exp[i])) + " " + str(coef[i]) + " " + str(x_val)+ " " + str(exp[i]))
+        new_term = derive_power(coef, exp)
+        coef = new_term[0]
+        exp = new_term[1]
+        val.append(eval)
+    return val
+
+''' 
+Returns an array of all orders of derivative
+[ [][], [][], [][]] - the returned array will have two arrays in one element
+the first array element is th coeficient and the second is the exponents in respective order
+example:
+2x^3 +3x -1
+[[6, 3, 0] [3, 1, 0], [18, 3, 0] [2, 0, -1], [36, 1, 0] [1, -1, -2]]
+'''
+def get_all_derivative(coef, exp):
+    val= []
+    for i in range(len(coef)):
+        new_term = derive_power(coef, exp)
+        coef = new_term[0]
+        exp = new_term[1]
+        v = [coef, exp]
+        val.append(v)
+    return val
